bfa7fc1989a991b153b6bdca8bf76acc324ff6de
Devin <60376349+Devinwhite37@users.noreply.github.com>
2021-10-18 02:30:51 -0400

    Added more queries to sort data on react app

--- SQL files/QueryData.sql
+++ SQL files/QueryData.sql
@@ -3,35 +3,62 @@
 * QueryData.sql will show the queries that will be used
 * on the react app for sorting comments, posts, and categories.
 * also shows data joines between tables
-* 
-* Still need many more queries.
 *
-* @version  1.0.0
+* @version  1.0.1
 * @since    10-13-2021
 * @author   Devin White
-*/
+**/
 
-
---Sort by votes
+--Sort Comments by votes
 SELECT * FROM Comment_tbl ORDER BY CommentVotes DESC;
 
 --Sort by oldest 
-SELECT * FROM Comment_tbl ORDER BY CommentID DESC;
+SELECT * FROM Comment_tbl WHERE CommentID = 2 ORDER BY CommentID DESC;
 
 --Sort by tags
-SELECT * FROM Comment_tbl ORDER BY CommentTags DESC;
+SELECT * FROM Comment_tbl WHERE PostID_comment = 6 ORDER BY CommentTags DESC;
 
---Shows all posts within a category
-SELECT CategoryID, CategoryName, PostTitle
+--Shows categories (homepage)
+SELECT CategoryName
+FROM Category_tbl;
+
+--Shows all posts within a category newest first(default, category page)
+SELECT PostTitle, PostVotes
 FROM Category_tbl, Post_tbl
-WHERE CategoryID = CategoryID_Post;
+WHERE CategoryID = CategoryID_Post
+AND CategoryID = 1
+ORDER BY PostID DESC;
 
---Shows all comments within a post
-SELECT PostID, PostTitle, Comment
+--Shows all posts within category by postvotes or popularity
+SELECT PostTitle, PostVotes
+FROM Category_tbl, Post_tbl
+WHERE CategoryID = CategoryID_Post
+AND CategoryID = 1
+ORDER BY PostVotes DESC;
+
+--Default(oldest first) Shows all comments within a post (6)
+SELECT CommentTags, Comment, CommentVotes
 FROM Post_tbl, Comment_tbl
-WHERE PostID = PostID_Comment;
+WHERE PostID = PostID_Comment
+AND PostID = 6;
+
+--All comments sorted by votes in specific post (6)
+SELECT CommentTags, Comment, CommentVotes
+FROM Post_tbl, Comment_tbl
+WHERE PostID = PostID_Comment
+AND PostID = 6
+ORDER BY CommentVotes;
+
+--All comments sorted by tags in specific post (6)
+SELECT CommentTags, Comment, CommentVotes
+FROM Post_tbl, Comment_tbl
+WHERE PostID = PostID_Comment
+AND PostID = 6
+ORDER BY CommentTags;
 
 --Shows all comments that were made by a user
 SELECT CommentID, Comment, UserName
 FROM Comment_tbl, User_tbl
-WHERE CommenterID = UserID, UserID=1;
+WHERE CommenterID = UserID;
+
+
