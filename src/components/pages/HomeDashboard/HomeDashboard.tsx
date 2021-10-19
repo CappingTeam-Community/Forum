c97ed9658fd3a4f394d96419bc27677506c26be6
ZacharyFredeman <zachary.fredeman1@marist.edu>
2021-10-19 10:23:48 -0400

    Homedashboard buttons, needs linking to interment page

--- src/components/pages/HomeDashboard/HomeDashboard.tsx
+++ src/components/pages/HomeDashboard/HomeDashboard.tsx
@@ -1,17 +1,48 @@
 import styles from './HomeDashboard.module.css';
-import React from "react";
 import { Button } from '@mui/material';
+import * as react from 'react';
+import { NavLink } from "react-router-dom";
+
 
 
 function HomeDashboard() {
     //Logic
 
+
     //Template
     return (
         <div className={styles.HomeDashboard}>
-            HomeDashboard Component
-            <Button variant="contained">Example of MUI</Button>
+
+            <h1> Categories</h1>
+
+            <div className={styles.Categories1}>
+                <div className={styles.Catbutton}>
+                    <Button variant="contained">Sports</Button>
+                </div>
+                <div className={styles.Catbutton}>
+                    <Button variant="contained">Robots</Button>
+                </div>
+                <div className={styles.Catbutton}>
+                    <Button variant="contained">Recipes</Button>
+                </div>
+
+            </div>
+
+            <div className={styles.Categories2}>
+                <div className={styles.Catbutton}>
+                    <Button variant="contained">Games</Button>
+                </div>
+                <div className={styles.Catbutton}>
+                    <Button variant="contained">Soups</Button>
+                </div>
+                <div className={styles.Catbutton}>
+                    <Button variant="contained">Fitness</Button>
+                </div>
+
+            </div>
         </div>
-    )};
+
+    )
+};
 
 export default HomeDashboard;
