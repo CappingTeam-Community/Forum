10777534ee9c213a025f53fb1fcf7d31cc323310
ZacharyFredeman <zachary.fredeman1@marist.edu>
2021-10-19 16:13:48 -0400

    fixing dashboard

---
+++ src/components/pages/IntermittenPost/IntermittenPost.tsx
@@ -0,0 +1,15 @@
+import styles from './IntermittenPost.module.css';
+
+function IntermittenPost() {
+    //Logic
+
+
+    //Template
+    return (
+        <div className={styles.Login}>
+            Intermitten Post Page
+        </div>
+        )
+};
+
+export default IntermittenPost;
