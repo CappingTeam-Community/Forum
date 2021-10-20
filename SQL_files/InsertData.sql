a4390d119dd9e5c4fde81b64e08be18219128a4f
Devin <60376349+Devinwhite37@users.noreply.github.com>
2021-10-20 17:06:50 -0400

    Updated created ids

--- SQL_files/InsertData.sql
+++ SQL_files/InsertData.sql
@@ -3,7 +3,7 @@
 * InsertData.sql will hard insert some sample data
 * to display on web application
 *
-* @version  1.0.1
+* @version  1.0.2
 * @since    10-12-2021
 * @author   Devin White
 **/
@@ -51,18 +51,18 @@ SELECT * FROM Category_tbl;
 INSERT INTO Post_tbl
 (CategoryID_Post, PostTitle, PostBody, PostDate, PostVotes, CreatorID)
 VALUES  (1, 'Why is Shang Chi Disney Plus release date November 11th, when they said only 45 Days of Exclusivity?',
-'The movie came out September 3rd. Thats more like 70 days ?',"2021-10-11", 1, 6),
+'The movie came out September 3rd. Thats more like 70 days ?',"2021-10-11", 1, 7),
         (1, 'Before the advent of the MCU who would you say were the A-list, B-list, C-list, and D-list characters of Marvel Comics?',
         "People often point to how Iron Man was an 'obscure' character before the movie, but I don't think that's completely accurate. That said I thought it would be insightful to take a step back through time and determine who the A-listers, B-listers, etc. were before the MCU took lesser-known characters and popularized them. I admit that I was not into Marvel much until around the first Avengers movie, so what I'm saying is largely based on second-hand experiences and what I can vaguely remember before Iron Man came out. Here is what I generally think the hierarchy of Marvel was: A-LIST - Spider-Man, X-Men, Fantastic Four, Hulk. Basically all of the heroes who regularly got cartoons, movies, merchandise, etc. and were front and center on posters. B-LIST - Iron Man, Captain America, Thor, Daredevil, and a few others that weren't as exposed in the public eye but were still very much prevalent in Marvel media, just usually as supporting roles and not the main focus. Though I'd say Cap and Daredevil sort of rode the line between A and B-list. C-LIST - Ant-Man, Doctor Strange, Black Panther, Captain Marvel, and most of the characters introduced after Phase 1 in the MCU. Fans of comics almost certainly knew who they were but they were mostly obscure in the eyes of the general public. D-LIST - Once you get to this point it's hard to really determine which characters belong here, but what I generally consider 'D-list' are the characters who even some comic fans didn't know anything about. I would like to say groups like the Guardians of the Galaxy or the Eternals, but I actually think those were too obscure to even justify calling them D-list; they were basically off the radar entirely. I'm not sure what to call this but there also seemed to be this weird area where Punisher and Blade were where they're pretty well-known to the public but weren't traditionally associated with Marvel heroes. Blade was a massively successful action-horror franchise but even today I have met people who didn't seem to know it was a Marvel movie, and at this point Punisher's skull symbol has taken on a completely new life as a mascot for the 'extreme edge goth punk' crowd, and an unsettling amount of soldiers and police officers, which is kind of scary. I admit that I was not into Marvel much until around the first Avengers movie, so what I'm saying is largely based on second-hand experiences and what I can vaguely remember before Iron Man came out. Here is what I generally think the hierarchy of Marvel was: A-LIST - Spider-Man, X-Men, Fantastic Four, Hulk. Basically all of the heroes who regularly got cartoons, movies, merchandise, etc. and were front and center on posters. B-LIST - Iron Man, Captain America, Thor, Daredevil, and a few others that weren't as exposed in the public eye but were still very much prevalent in Marvel media, just usually as supporting roles and not the main focus. Though I'd say Cap and Daredevil sort of rode the line between A and B-list. C-LIST - Ant-Man, Doctor Strange, Black Panther, Captain Marvel, and most of the characters introduced after Phase 1 in the MCU. Fans of comics almost certainly knew who they were but they were mostly obscure in the eyes of the general public. D-LIST - Once you get to this point it's hard to really determine which characters belong here, but what I generally consider 'D-list' are the characters who even some comic fans didn't know anything about. I would like to say groups like the Guardians of the Galaxy or the Eternals, but I actually think those were too obscure to even justify calling them D-list; they were basically off the radar entirely. I'm not sure what to call this but there also seemed to be this weird area where Punisher and Blade were where they're pretty well-known to the public but weren't traditionally associated with Marvel heroes. Blade was a massively successful action-horror franchise but even today I have met people who didn't seem to know it was a Marvel movie, and at this point Punisher's skull symbol has taken on a completely new life as a mascot for the 'extreme edge goth punk' crowd, and an unsettling amount of soldiers and police officers, which is kind of scary. Does this sound about right? If not, who would you rank as A-list, B-list, etc. before the inception of the Marvel Cinematic Universe?",
 '2021-10-13',
-6,
+27,
 2),
         (1, "So who all is dead as of right now?", 'I have seen most of the MCU films once but none twice and some never so I am a really casual viewer.
 After watching what feels like a dozen movies over the last several years (some out of order) and with the time-travel shenanigans, multiverses, the Snap and Blip, and fact that the films themselves are set in different “time periods” of the MCU, I don’t know which heroes are dead and which aren’t as of “present day” MCU.
 After watching what feels like a dozen movies over the last several years (some out of order) and with the time-travel shenanigans, multiverses, the Snap and Blip, and fact that the films themselves are set in different “time periods” of the MCU, I don’t know which heroes are dead and which aren’t as of “present day” MCU.
-Tony Stark, I think… and I remember Captain America getting real old and retiring after time-travel stuff so I guess he is dead too? Anyone else?', '2021-10-13', 20, 6),
-        (1, "Adam Warlock vs Captain Marvel", "As you know, it is already known who will be the actor who will give life to the character of Adam, who do you think would win?", "2021-10-14", 1, 2),
-        (1, "What were Thor's last words to Loki", "I heard people said It was 'You really are the worst brother', but I don't see him say that in Infinity War or Thor Ragnarok.", '2021-10-14', 11, 3),
+Tony Stark, I think… and I remember Captain America getting real old and retiring after time-travel stuff so I guess he is dead too? Anyone else?', '2021-10-13', 20, 17),
+        (1, "Adam Warlock vs Captain Marvel", "As you know, it is already known who will be the actor who will give life to the character of Adam, who do you think would win?", "2021-10-14", 1, 18),
+        (1, "What were Thor's last words to Loki", "I heard people said It was 'You really are the worst brother', but I don't see him say that in Infinity War or Thor Ragnarok.", '2021-10-14', 11, 24),
         (1, 'MCU "STRONGEST AVENGER" list', 'Also, lets break down who is eligible for this:
 "The original 6" (Iron Man, Captian America, Thor, Hulk, Black Widow, and Hawkeye)
 Guardians Of The Galaxy (Star-Lord, Gamora, Drax, Groot, Rocket, Nebula, and Mantis)
@@ -98,7 +98,7 @@ I picked the based on the final Endgame scene and/or if they have had a big enou
 The rule is that their characters are based on the MCU versions, not comic book versions. If there is no information, then a fallback to the comics is allowed. Also, anything the filmmakers say (even if contradicting the comics and/or the films) also is taken into account.
 Feel free to make your top 5, top 10, top 23, etc. list if you dont want to do all 23
 Also, we are comparing to their final Endgame versions: Thor with only Stormbreaker, Endgame Hulk, etc.
-Good luck', "2021-10-14", 1, 8),
+Good luck', "2021-10-14", 1, 19),
         (2, 'League Esports Funny Joke', 'Hey guys I was watching worlds and I came up with this joke
 What do you call it when you randomly start crying for no reason and you cant figure out why?
 RNG Cryin', "2021-10-20", 1, 6),
